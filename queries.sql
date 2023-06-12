e2c37fd1d2ac886a2594e11c73d35998b5fb4f03
jenniferkelly <jenbilyeu@yahoo.com>
2023-06-12 06:28:21 -0500

    Assignment4

--- queries.sql
+++ queries.sql
@@ -19,7 +19,8 @@
 DROP TABLE job;
 -- Part 4:
 
-SELECT * FROM skill,
-INNER JOIN job_skills ON job_skills.skills_id += skill.id,
-WHERE job_skills.jobs_id IS NOT NULL,
+SELECT *
+FROM skill
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
